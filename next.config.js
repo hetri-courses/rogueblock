35342a8cfbc30ad396016924916aec978a17cf5c
hetri-courses <owner@hetri.org>
2025-11-28 11:15:43 -0800

    Fix GitHub Pages paths: add basePath and assetPrefix, fix homepage links

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const nextConfig = {
   trailingSlash: process.env.NODE_ENV === 'production',
   images: {
     unoptimized: process.env.NODE_ENV === 'production'
-  }
+  },
+  basePath: '/rogueblock',
+  assetPrefix: '/rogueblock/'
 }
 
 module.exports = nextConfig
